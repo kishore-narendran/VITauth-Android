3b363f2c381137c00335d06c3e25ea3e67a0f019
kishore-narendran <kishore.narendran09@gmail.com>
2015-04-05 10:33:32 +0530

    Changed location of listview and cleaned up code. Tested

--- app/src/main/java/app/vit/vitauth/fragment/StudentsFragment.java
+++ app/src/main/java/app/vit/vitauth/fragment/StudentsFragment.java
@@ -1,30 +1,29 @@
 package app.vit.vitauth.fragment;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
 import app.vit.vitauth.R;
-import app.vit.vitauth.adapters.ListStudentsAdapter;
-import app.vit.vitauth.list_items.ListItemStudent;
 
 public class StudentsFragment extends Fragment {
 
     public ListStudentsAdapter myListStudentsAdapter;
-
     public StudentsFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         //Generating static data for testing
         ListItemStudent students[] = new ListItemStudent[6];
         students[0] = new ListItemStudent("11BCE0094", "Kishore Narendran");
@@ -38,10 +37,59 @@ public class StudentsFragment extends Fragment {
         myListStudentsAdapter = new ListStudentsAdapter(getActivity(), students1);
 
         View rootView = inflater.inflate(R.layout.fragment_students, container, false);
-
         ListView listView = (ListView) rootView.findViewById(R.id.listview_students);
         listView.setAdapter(myListStudentsAdapter);
 
         return rootView;
+    }
+
+    public class ListStudentsAdapter extends ArrayAdapter<ListItemStudent> {
+
+        private final Context context;
+        private final ArrayList<ListItemStudent> itemsArrayList;
+
+        public ListStudentsAdapter(Context context, ArrayList<ListItemStudent> itemsArrayList) {
+
+            super(context, R.layout.list_item_students, itemsArrayList);
+
+            this.context = context;
+            this.itemsArrayList = itemsArrayList;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater inflater = (LayoutInflater) context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            View rowView = inflater.inflate(R.layout.list_item_students, parent, false);
+
+            TextView regnoView = (TextView) rowView.findViewById(R.id.list_item_regno);
+            TextView nameView = (TextView) rowView.findViewById(R.id.list_item_name);
+
+            regnoView.setText(itemsArrayList.get(position).getRegno());
+            nameView.setText(itemsArrayList.get(position).getName());
+
+            return rowView;
+        }
+    }
+
+    public class ListItemStudent {
+
+        String regno;
+        String name;
+
+        public ListItemStudent(String regno, String name) {
+            this.regno = regno;
+            this.name = name;
+        }
+
+        public String getRegno() {
+            return regno;
+        }
+
+        public String getName() {
+            return name;
+        }
+
     }
 }
