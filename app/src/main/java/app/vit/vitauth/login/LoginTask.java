45d10b9200c97e2e0be45bb348a7e8b54dbc93d2
Aarthy KC <kcaarthy@gmail.com>
2015-04-22 14:36:19 +0530

    Post request created and response logged

--- app/src/main/java/app/vit/vitauth/login/LoginTask.java
+++ app/src/main/java/app/vit/vitauth/login/LoginTask.java
@@ -6,11 +6,15 @@ import android.util.Log;
 
 import com.google.gson.Gson;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.URL;
 
 import app.vit.vitauth.exam.ExamActivity;
-import data.Class;
 import data.ExamInfo;
 import data.GetExamInfo;
 
@@ -35,17 +39,41 @@ public class LoginTask extends AsyncTask<GetExamInfo, Void, ExamInfo> {
         Log.i("JSON STRING", json);
         HttpURLConnection urlConnection = null;
         BufferedReader reader = null;
-
         try {
-            final String VITAUTH_BASE_URL = "http://vitauth.herokuapp.com/api/client/login";
-            final String EMPID_PARAM = "empid";
-            final String PASSWORD_PARAM = "password";
+            final String VITAUTH_BASE_URL = "http://vitauth.herokuapp.com/api/client/GetExamInfo";
+            byte[] postData = json.getBytes();
+            int postDataLength = postData.length;
+            URL url = new URL(VITAUTH_BASE_URL);
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setDoOutput(true);
+            urlConnection.setDoInput(true);
+            urlConnection.setRequestMethod("POST");
+            urlConnection.setRequestProperty("Content-Type", "application/json");
+            urlConnection.setRequestProperty("Content-Length", Integer.toString(postDataLength));
+            urlConnection.setUseCaches(false);
+            try (DataOutputStream wr = new DataOutputStream(urlConnection.getOutputStream())) {
+                wr.write(postData);
+                wr.flush();
+                wr.close();
+            }
 
-            // TODO get and parse JSON
+            InputStream responseStream = new BufferedInputStream(urlConnection.getInputStream());
+            BufferedReader responseStreamReader = new BufferedReader(new InputStreamReader(responseStream));
+            String line = "";
+            StringBuilder stringBuilder = new StringBuilder();
+            while ((line = responseStreamReader.readLine()) != null) {
+                stringBuilder.append(line);
+            }
+            responseStreamReader.close();
 
-            examInfo = new ExamInfo("WS", "CATI", "A1", "SJT301", "4:00PM", new Class[5]);
+            String response = stringBuilder.toString();
+            Log.i("Response", response);
+            gson = new Gson();
+            ExamInfo examInfo1 = gson.fromJson(response, ExamInfo.class);
+            return examInfo1;
         }
-        catch (Exception e) {
+       catch (Exception e) {
+            Log.e("Error", e.getMessage());
 
         }
         finally {
